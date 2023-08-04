d6f8039426b46c54330e3748c6f5f31ecbe7cd1c
+ <andrewjohnjulian@gmail.com>
2023-08-04 14:27:57 -0600

    ahhh

--- app/page.js
+++ app/page.js
@@ -4,11 +4,12 @@ import { BiLogoPython } from "react-icons/bi";
 import { FcCollaboration } from "react-icons/fc";
 import { LiaKaggle } from "react-icons/lia";
 import { TbBrowserCheck } from "react-icons/tb";
+import { RiSurveyLine } from "react-icons/ri";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-row gap-[100px] h-screen justify-center content-center align-middle items-center">
+    <div className="grid grid-cols-3 mx-auto gap-[50px] w-[80%] h-screen justify-center content-center align-middle items-center">
       <div className="flex flex-col p-4 content-center justify-center items-center">
         <Link
           href="https://www.canva.com/design/DAFIJFUKD_E/dwYosyGScPzY65oFVvy5Mg/edit?utm_content=DAFIJFUKD_E&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
@@ -19,7 +20,6 @@ export default function Home() {
         </Link>
         <p className="text-4xl font-bold">Secret Files</p>
       </div>
-
       <div className="flex flex-col p-4 content-center justify-center items-center">
         <Link
           href="https://replit.com/teams/join/yboidqvwryaptmpqnjvwgxcozjstyahk-SummerCamp2023"
@@ -56,6 +56,15 @@ export default function Home() {
         </Link>
         <p className="text-4xl font-bold text-center">Teachable</p>
       </div>
-    </main>
+      <div className="flex flex-col p-4 content-center justify-center items-center">
+        <Link
+          href="https://forms.gle/JysaCe8TXXPzfBqv5"
+          target="_blank"
+        >
+          <RiSurveyLine className="text-9xl" />
+        </Link>
+        <p className="text-4xl font-bold text-center">Survey</p>
+      </div>
+    </div>
   );
 }
